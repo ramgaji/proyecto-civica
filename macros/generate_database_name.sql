a72d78512136b525296fd63917cbed3338d2c18a
Ramón García <ramgaji@gmail.com>
2026-05-09 19:00:47 +0000

    yml principales

---
+++ macros/generate_database_name.sql
@@ -0,0 +1,8 @@
+--macros/generate_database_name.sql--nombre EXACTO obligatorio
+    {% macro generate_schema_name(custom_schema_name, node) -%}
+        {%- if custom_schema_name is not none -%}
+            {{ custom_schema_name | trim }}
+        {%- else -%}
+            {{ target.schema | trim }}
+        {%- endif -%}
+    {%- endmacro %}
